3b039e7bd6173a138dd71ab03427c7a8a19a31d1
vijay kumar <308vijaykumar@gmail.com>
2023-02-01 01:18:34 +0530

    props add in projectCard

--- src/components/projects-card/ProjectCard.jsx
+++ src/components/projects-card/ProjectCard.jsx
@@ -7,10 +7,10 @@ export default function ProjectCard(props) {
             <img src={props.image} alt="featuredImage" className="img-responsive"/>
         </div>
         <div className="project-card-contant">
-            <h2 className="project-title">Relationship</h2>
-            <p className="project-descp">dignissimos natus omnis voluptatem esse fugit placeat! Odio explicabo obcaecati molestias maxime, facere sit possimus? Dignissimos dolor voluptatibus quo sint sunt quia ullam. Nisi, assumenda fugit!</p>
+            <h2 className="project-title">{props.projectTitle}</h2>
+            <p className="project-descp">{props.projectDescription}</p>
             <div className="readmore-duration">
-                <p className="project-duration">1 Weeks</p>
+                <p className="project-duration">{props.duration}</p>
                 <button className="readmore-btn">Read More...</button>    
             </div>
         </div>
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -14,23 +14,42 @@ export default function  Projects() {
       <div className="project-wrapper">
        <ProjectCard
           image={timeCalculator}
+          projectTitle="Time Calculator"
+          projectDescription="Time calculator is a simple app to calculate 5 days time. Basically it is used for working professinal who's has to complete their time for the week."
+          duration="2 Weeks"
+
         />
 
         <ProjectCard
           image={lavulator}
+          projectTitle="Relationship"
+          projectDescription="This Project is for check the relationship. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam"
+          duration="1 Week"
         />
         <ProjectCard
           image={clgSite}
+          projectTitle="College Site"
+          projectDescription="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis "
+          duration="3 Weeks"
         />
        
         <ProjectCard
           image={chooseOne}
+          projectTitle="Choose One"
+          projectDescription="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis "
+          duration="3 Weeks"
         />
         <ProjectCard
           image={gameProject}
+          projectTitle="Gass The Numbar"
+          projectDescription="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis "
+          duration="2 Weeks"
         />
         <ProjectCard
           image={eboots}
+          projectTitle="Web Template clone"
+          projectDescription="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis "
+          duration="1 Week"
         />
       </div>
     </div>
